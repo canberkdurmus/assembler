344e5984267a20f5897e103219a239426c1a43c4
canberkdurmus <canberkdurmus@gmail.com>
2019-11-19 22:13:47 +0300

    Nobody is going to read commit message, I love her so much xd

--- Instruction.py
+++ Instruction.py
@@ -1,32 +1,95 @@
-def add(opcode, args):
-    print("i")
+register_dic = {
+    "R0": "0000",
+    "R1": "0001",
+    "R2": "0010",
+    "R3": "0011",
+    "R4": "0100",
+    "R5": "0101",
+    "R6": "0110",
+    "R7": "0111",
+    "R8": "1000",
+    "R9": "1001",
+    "RA": "1010",
+    "RB": "1011",
+    "RC": "1100",
+    "RD": "1101",
+    "RE": "1110",
+    "RF": "1111",
+}
 
-def add(opcode, args):
-    print("i")
+register_hex_dic = {
+    "R0": "0",
+    "R1": "1",
+    "R2": "2",
+    "R3": "3",
+    "R4": "4",
+    "R5": "5",
+    "R6": "6",
+    "R7": "7",
+    "R8": "8",
+    "R9": "9",
+    "RA": "a",
+    "RB": "b",
+    "RC": "c",
+    "RD": "d",
+    "RE": "e",
+    "RF": "f",
+}
+
+
+def add_args(args):
+    arg_str = register_hex_dic[args[0]]
+    arg_str += register_hex_dic[args[1]]
+    arg_str += register_hex_dic[args[2]]
+    return arg_str
+
+
+def addi_args(args):
+    arg_str = str(register_hex_dic[args[0]])
+    arg_str += register_hex_dic[args[1]]
+    arg_str += args[2]  # str(hex(args[2])[2:])
+    return arg_str
+
+
+def jump_offset(offset):
+    if len(offset) == 1:
+        offset += "0"
+        offset += offset
+    elif len(offset) == 2:
+        offset += offset
+    return offset
 
 
 class Instruction:
-    opcode = None
+    opcode = ""
     args = []
-
-    def identify_opcode(opcode, args):
-        def switch_demo(opcode):
-            switcher = {
-                "0000": add(opcode, args),
-                2: "0001",
-                3: "0010",
-                4: "0011",
-                5: "0100",
-                6: "0101",
-                7: "0111",
-                8: "1000",
-                9: "1001",
-                10: "1010",
-                11: "1011",
-                12: "1111"
-            }
+    hex = ""
 
     def __init__(self, opcode, args):
         self.opcode = opcode
         self.args = args
-        self.identify_opcode(opcode, args)
+        self.convert_line()
+
+    def convert_line(self):
+        hex_str = ""
+        opcode = self.opcode
+
+        if opcode == "0" or opcode == "1" or opcode == "2" or opcode == "3":
+            hex_str += opcode
+            hex_str += add_args(self.args)
+
+        elif opcode == "4" or opcode == "5" or opcode == "6" or opcode == "7" or opcode == "b" or opcode == "c" or opcode == "d" or opcode == "e" or opcode == "f":
+            hex_str += opcode
+            hex_str += addi_args(self.args)
+        elif opcode == "8":
+            hex_str += opcode
+            offset = str(hex(int(self.args[0]))[2:])
+            print("jump offset is ", offset)
+            hex_str += jump_offset(offset)
+        elif opcode == "9" or opcode == "a":
+            hex_str += opcode
+            hex_str += register_hex_dic[self.args[0]]
+            offset = str(hex(int(self.args[1]))[2:])
+            hex_str += jump_offset(offset)
+
+        self.hex = hex_str
--- assembler.py
+++ assembler.py
@@ -1,3 +1,5 @@
+from Instruction import Instruction
+
 opcode_dict = {
     "ADD":  "0000",
     "AND":  "0001",
@@ -17,14 +19,37 @@ opcode_dict = {
     "BGE":  "1111",
 }
 
+opcode_hex_dict = {
+    "ADD":  "0",
+    "AND":  "1",
+    "OR":   "2",
+    "XOR":  "3",
+    "ADDI": "4",
+    "ANDI": "5",
+    "ORI":  "6",
+    "XORI": "7",
+    "JUMP": "8",
+    "LD":   "9",
+    "ST":   "a",
+    "BEQ":  "b",
+    "BLT":  "c",
+    "BGT":  "d",
+    "BLE":  "e",
+    "BGE":  "f",
+}
+
 f = open('code.txt', "r")
 lines = f.readlines()
 f.close()
 
+instruction_arr = []
+i = 0
+
 for line in lines:
     line_arr = line.split()
-    opcode = opcode_dict[line_arr[0]]
+    opcode = opcode_hex_dict[line_arr[0]]
     args = line_arr[1].split(',')
-    print(opcode)
-    print(args)
+    instruction_arr.append(Instruction(opcode, args))
+    print(instruction_arr[i].hex)
+    i += 1
 
